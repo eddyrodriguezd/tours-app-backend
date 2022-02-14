373b6e9983cda51928ca8db044b5a2147503fcf2
Joao Moran <moranrosales23@hotmail.com>
2022-02-13 21:50:01 -0500

    add/list categories

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-require('dotenv').config({ path: '.env' });
-require('./config/dbConfig');
+require("dotenv").config({ path: ".env" });
+require("./config/dbConfig");
 
 //Load routes
-const reservationRoutes = require('./routes/reservation');
+const reservationRoutes = require("./routes/reservation");
+const categoriesRoutes = require("./routes/categories");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log('App listening on port', process.env.PORT || 8080);
+  console.log("App listening on port", process.env.PORT || 8080);
 });
 
 //Router
 app.use(`/api/reservation`, reservationRoutes);
+app.use("/api", categoriesRoutes);
 
 //module.exports = app;
---
+++ services/CategoriesService.js
@@ -0,0 +1,10 @@
+const CategoriesSchema = require("../models/Categories");
+
+const add = ({ description }) => CategoriesSchema.create({ description });
+
+const getAll = () => CategoriesSchema.find();
+
+module.exports = {
+  add,
+  getAll,
+};
