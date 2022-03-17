ddacb30fc1393b1c45e82913dfead4eeeb5692b5
Eddy Rodriguez <ext_eddrodriguez@falabella.cl>
2022-03-17 14:19:23 -0500

    Configured service to fetch hotels' info from external API

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 var cors = require("cors");
 const cookieParser = require("cookie-parser");
+
 const reservation = require("./routes/reservation");
+const hotel = require("./routes/hotel");
 const user = require("./routes/user");
 const categories = require("./routes/categories");
 const tours = require("./routes/tour");
@@ -19,6 +21,8 @@ app.use(cors(corsOptions));
 
 
 app.use("/api/reservation", reservation);
+app.use("/api/hotel", hotel);
+
 app.use("/api/", user);
 app.use("/api/category", categories);
 app.use("/api/tour", tours);
